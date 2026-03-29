5e5b01186e47262fd476fd020b03d060a21b497d
Renato <renatorios60@gmail.com>
2026-03-29 15:13:27 -0600

    nueva version 0.2.3(arreglo de colores 2)

--- controllers/alexa_controller.py
+++ controllers/alexa_controller.py
@@ -3,24 +3,37 @@ from views.apl_templates import pantalla_principal
 from services.alexa_service import procesar_accion
 
 def manejar_request(data):
-    tipo = data["request"]["type"]
+    interfaces = data["context"]["System"]["device"]["supportedInterfaces"]
 
-    # 🟢 Launch
-    if tipo == "LaunchRequest":
+    tiene_apl = "Alexa.Presentation.APL" in interfaces
+
+    request_type = data["request"]["type"]
+
+    # 🔥 CUANDO ABRES LA SKILL
+    if request_type == "LaunchRequest":
+
+        if tiene_apl:
+            mensaje = "Detecté pantalla"
+        else:
+            mensaje = "No detecté pantalla"
+
         return jsonify({
             "version": "1.0",
             "response": {
-            "outputSpeech": {
-                "type": "PlainText",
-                "text": "Estamos en Asistente C"
-            },
-            "directives": [
-                {
-                    "type": "Alexa.Presentation.APL.RenderDocument",
-                    "token": "main",
-                    "document": pantalla_principal()
-                }
-                ],
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": mensaje
+                },
+
+                # 👇 SOLO si hay pantalla mandamos UI
+                "directives": [
+                    {
+                        "type": "Alexa.Presentation.APL.RenderDocument",
+                        "token": "main",
+                        "document": pantalla_principal()
+                    }
+                ] if tiene_apl else [],
+
                 "shouldEndSession": False
             }
         })
