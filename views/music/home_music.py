1f7c7f9a139662b48fb47f2bd29a8fedb67b6444
Renato <renatorios60@gmail.com>
2026-05-04 18:23:52 -0600

    Nueva interfaz home_music 1

--- views/music/home_music.py
+++ views/music/home_music.py
@@ -6,69 +6,243 @@ def music_gui():
             "parameters": ["payload"],
             "items": [
                 {
-                    # CONTENEDOR PRINCIPAL (layout)
                     "type": "Container",
                     "width": "100%",
                     "height": "100%",
-                    "justifyContent": "center",
-                    "alignItems": "center",
-                    
                     "items": [
                         {
-                            # FRAME = FONDO REAL
                             "type": "Frame",
                             "width": "100%",
                             "height": "100%",
                             "backgroundColor": "#780000",
-
                             "items": [
                                 {
-                                    # CONTENIDO CENTRADO
-                                    "type": "Container",
-                                    "direction": "column",
-                                    "width": "100%",
-                                    "height": "100%",
-                                    "justifyContent": "center",
+                                    # BORDE
+                                    "type": "Frame",
+                                    "width": "90%",
+                                    "height": "85%",
+                                    "borderWidth": "3dp",
+                                    "borderColor": "#FFFFFF",
+                                    "alignSelf": "center",
                                     "alignItems": "center",
 
                                     "items": [
 
-                                        # TÍTULO
                                         {
-                                            "type": "Text",
-                                            "text": "Bienvenido a Música",
-                                            "fontSize": "60dp",
-                                            "color": "#1B1B1C",
-                                            "marginBottom": "40dp"
+                                            "type": "Container",
+                                            "height": "40dp"
                                         },
 
-                                        # BOTÓN APRENDER
+                                        # TITULO
                                         {
-                                            "type": "TouchWrapper",
-                                            "onPress": {
-                                                "type": "SendEvent",
-                                                "arguments": ["boton1"]
-                                            },
-                                            "item": {
-                                                "type": "Frame",
-                                                "width": "70%",
-                                                "height": "60dp",
-                                                "backgroundColor": "#A50000",
-                                                "borderRadius": "25dp",
-                                                "justifyContent": "center",
-                                                "alignItems": "center",
+                                            "type": "Frame",
+                                            "width": "70%",
+                                            "height": "70dp",
+                                            "backgroundColor": "#E6E6E6",
+                                            "borderRadius": "40dp",
+                                            "justifyContent": "center",
+                                            "alignItems": "center",
+                                            "items": [
+                                                {
+                                                    "type": "Text",
+                                                    "text": "CALMA TU ALMA CON UN POCO DE MÚSICA",
+                                                    "fontSize": "26dp",
+                                                    "color": "#000000",
+                                                    "textAlign": "center",
+                                                    "width": "90%"
+                                                }
+                                            ]
+                                        },
 
-                                                # TEXTO DEL BOTÓN
-                                                "items": [
-                                                    {
-                                                        "type": "Text",
-                                                        "text": "Fase 1",
-                                                        "fontSize": "30dp",
-                                                        "color": "#FFFFFF"
-                                                    }
-                                                ]
-                                            }
+                                        {
+                                            "type": "Container",
+                                            "height": "50dp"
+                                        },
+
+                                        # CONTENEDOR PRINCIPAL
+                                        {
+                                            "type": "Container",
+                                            "direction": "row",
+                                            "justifyContent": "space-around",
+                                            "width": "100%",
+                                            "items": [
+
+                                                # 🔴 DISCO 1
+                                                {
+                                                    "type": "Container",
+                                                    "direction": "column",
+                                                    "alignItems": "center",
+                                                    "items": [
+
+                                                        # CIRCULO GRANDE
+                                                        {
+                                                            "type": "Frame",
+                                                            "width": "180dp",
+                                                            "height": "180dp",
+                                                            "backgroundColor": "#E6E6E6",
+                                                            "borderRadius": "90dp",
+                                                            "justifyContent": "center",
+                                                            "alignItems": "center",
+                                                            "items": [
+
+                                                                # CENTRO NEGRO
+                                                                {
+                                                                    "type": "Frame",
+                                                                    "width": "60dp",
+                                                                    "height": "60dp",
+                                                                    "backgroundColor": "#000000",
+                                                                    "borderRadius": "30dp"
+                                                                }
+
+                                                            ]
+                                                        },
+
+                                                        # BOTON
+                                                        {
+                                                            "type": "TouchWrapper",
+                                                            "onPress": {"type": "SendEvent","arguments": ["playlist_mama"]},
+                                                            "item": {
+                                                                "type": "Frame",
+                                                                "width": "180dp",
+                                                                "height": "50dp",
+                                                                "backgroundColor": "#CFE3E8",
+                                                                "borderRadius": "25dp",
+                                                                "marginTop": "20dp",
+                                                                "items": [{
+                                                                    "type": "Container",
+                                                                    "width": "100%",
+                                                                    "height": "100%",
+                                                                    "justifyContent": "center",
+                                                                    "alignItems": "center",
+                                                                    "items": [{
+                                                                        "type": "Text",
+                                                                        "text": "PLAYLIST MAMÁ",
+                                                                        "fontSize": "18dp",
+                                                                        "color": "#0E5678",
+                                                                        "width": "100%",
+                                                                        "textAlign": "center"
+                                                                    }]
+                                                                }]
+                                                            }
+                                                        }
+                                                    ]
+                                                },
+
+                                                # 🔴 DISCO 2
+                                                {
+                                                    "type": "Container",
+                                                    "direction": "column",
+                                                    "alignItems": "center",
+                                                    "items": [
+
+                                                        {
+                                                            "type": "Frame",
+                                                            "width": "180dp",
+                                                            "height": "180dp",
+                                                            "backgroundColor": "#E6E6E6",
+                                                            "borderRadius": "90dp",
+                                                            "justifyContent": "center",
+                                                            "alignItems": "center",
+                                                            "items": [
+                                                                {
+                                                                    "type": "Frame",
+                                                                    "width": "60dp",
+                                                                    "height": "60dp",
+                                                                    "backgroundColor": "#000000",
+                                                                    "borderRadius": "30dp"
+                                                                }
+                                                            ]
+                                                        },
+
+                                                        {
+                                                            "type": "TouchWrapper",
+                                                            "onPress": {"type": "SendEvent","arguments": ["playlist_renato"]},
+                                                            "item": {
+                                                                "type": "Frame",
+                                                                "width": "180dp",
+                                                                "height": "50dp",
+                                                                "backgroundColor": "#CFE3E8",
+                                                                "borderRadius": "25dp",
+                                                                "marginTop": "20dp",
+                                                                "items": [{
+                                                                    "type": "Container",
+                                                                    "width": "100%",
+                                                                    "height": "100%",
+                                                                    "justifyContent": "center",
+                                                                    "alignItems": "center",
+                                                                    "items": [{
+                                                                        "type": "Text",
+                                                                        "text": "PLAYLIST RENATO",
+                                                                        "fontSize": "18dp",
+                                                                        "color": "#0E5678",
+                                                                        "width": "100%",
+                                                                        "textAlign": "center"
+                                                                    }]
+                                                                }]
+                                                            }
+                                                        }
+                                                    ]
+                                                },
+
+                                                # 🔴 DISCO 3
+                                                {
+                                                    "type": "Container",
+                                                    "direction": "column",
+                                                    "alignItems": "center",
+                                                    "items": [
+
+                                                        {
+                                                            "type": "Frame",
+                                                            "width": "180dp",
+                                                            "height": "180dp",
+                                                            "backgroundColor": "#E6E6E6",
+                                                            "borderRadius": "90dp",
+                                                            "justifyContent": "center",
+                                                            "alignItems": "center",
+                                                            "items": [
+                                                                {
+                                                                    "type": "Frame",
+                                                                    "width": "60dp",
+                                                                    "height": "60dp",
+                                                                    "backgroundColor": "#000000",
+                                                                    "borderRadius": "30dp"
+                                                                }
+                                                            ]
+                                                        },
+
+                                                        {
+                                                            "type": "TouchWrapper",
+                                                            "onPress": {"type": "SendEvent","arguments": ["playlist_oliver"]},
+                                                            "item": {
+                                                                "type": "Frame",
+                                                                "width": "180dp",
+                                                                "height": "50dp",
+                                                                "backgroundColor": "#CFE3E8",
+                                                                "borderRadius": "25dp",
+                                                                "marginTop": "20dp",
+                                                                "items": [{
+                                                                    "type": "Container",
+                                                                    "width": "100%",
+                                                                    "height": "100%",
+                                                                    "justifyContent": "center",
+                                                                    "alignItems": "center",
+                                                                    "items": [{
+                                                                        "type": "Text",
+                                                                        "text": "PLAYLIST OLIVER",
+                                                                        "fontSize": "18dp",
+                                                                        "color": "#0E5678",
+                                                                        "width": "100%",
+                                                                        "textAlign": "center"
+                                                                    }]
+                                                                }]
+                                                            }
+                                                        }
+                                                    ]
+                                                }
+
+                                            ]
                                         }
+
                                     ]
                                 }
                             ]
