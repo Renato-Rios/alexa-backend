6ac21801b3970228e6d1f78caafc0581f18d1678
Renato <renatorios60@gmail.com>
2026-05-07 18:17:56 -0600

    Nueva interfaz fase 1

--- views/learn/fase_1.py
+++ views/learn/fase_1.py
@@ -3,35 +3,31 @@ def fase_1_gui():
         "type": "APL",
         "version": "1.7",
         "mainTemplate": {
-            "parameters": [
-                "payload"
-            ],
+            "parameters": ["payload"],
             "items": [
                 {
-                    "type": "Container",
+                    "type": "Frame",
                     "width": "100%",
                     "height": "100%",
-                    "direction": "column",
-                    "items": [
-                        {
-                            "type": "Frame",
-                            "width": "100%",
-                            "height": "100%",
-                            "backgroundColor": "#003049",
-                            "item": {
+                    "backgroundColor": "#003049",
+                    "item": {
+                        "type": "Container",
+                        "width": "100%",
+                        "height": "100%",
+                        "items": [
+                            # CONTENIDO CENTRAL (Imagen y Texto)
+                            {
                                 "type": "Container",
-                                "width": "100%",
-                                "height": "100%",
+                                "grow": 1, # Esto empuja los botones hacia abajo
                                 "justifyContent": "center",
                                 "alignItems": "center",
                                 "items": [
                                     {
                                         "type": "Image",
                                         "source": "${payload.datosFase.image}",
-                                        "width": "600dp",
-                                        "height": "400dp",
-                                        "scale": "best-fit",
-                                        "align": "center"
+                                        "width": "500dp",
+                                        "height": "350dp",
+                                        "scale": "best-fit"
                                     },
                                     {
                                         "type": "Text",
@@ -39,22 +35,71 @@ def fase_1_gui():
                                         "fontSize": "60dp",
                                         "fontWeight": "700",
                                         "color": "#FFFFFF",
-                                        "textAlign": "center",
-                                        "marginTop": "20dp"
+                                        "marginTop": "10dp"
                                     },
                                     {
                                         "type": "Text",
                                         "text": "${payload.datosFase.phonetic}",
-                                        "fontSize": "35dp",
+                                        "fontSize": "30dp",
                                         "color": "#CCCCCC",
-                                        "fontStyle": "italic",
-                                        "textAlign": "center",
-                                        "marginTop": "10dp"
+                                        "fontStyle": "italic"
+                                    }
+                                ]
+                            },
+                            # BARRA DE BOTONES INFERIOR
+                            {
+                                "type": "Container",
+                                "direction": "row",
+                                "width": "100%",
+                                "paddingLeft": "40dp",
+                                "paddingRight": "40dp",
+                                "paddingBottom": "20dp",
+                                "justifyContent": "spaceBetween",
+                                "items": [
+                                    # BOTÓN ATRÁS
+                                    {
+                                        "type": "TouchWrapper",
+                                        "onPress": {
+                                            "type": "SendEvent",
+                                            "arguments": ["aprender"] # Regresa al home de aprender
+                                        },
+                                        "item": {
+                                            "type": "Frame",
+                                            "backgroundColor": "#D62828",
+                                            "borderRadius": "10dp",
+                                            "padding": "15dp",
+                                            "item": {
+                                                "type": "Text",
+                                                "text": "ATRÁS",
+                                                "color": "white",
+                                                "fontWeight": "bold"
+                                            }
+                                        }
+                                    },
+                                    # BOTÓN CAMBIAR (SIGUIENTE)
+                                    {
+                                        "type": "TouchWrapper",
+                                        "onPress": {
+                                            "type": "SendEvent",
+                                            "arguments": ["fase1"] # Recarga la misma fase (traerá otra random)
+                                        },
+                                        "item": {
+                                            "type": "Frame",
+                                            "backgroundColor": "#F77F00",
+                                            "borderRadius": "10dp",
+                                            "padding": "15dp",
+                                            "item": {
+                                                "type": "Text",
+                                                "text": "CAMBIAR",
+                                                "color": "white",
+                                                "fontWeight": "bold"
+                                            }
+                                        }
                                     }
                                 ]
                             }
-                        }
-                    ]
+                        ]
+                    }
                 }
             ]
         }
